46a1d34eec78f284a3002e89aa5d39291653c96a
xiongxiongdjz <1165573190@qq.com>
2020-05-29 16:51:37 +0800

    Promise.all()

--- src/components/todolist.vue
+++ src/components/todolist.vue
@@ -42,53 +42,87 @@ export default {
   },
   methods: {
     access(content) {
-      // 新建资源.post
+      // 新建资源  create .post()
       axios
         .post("http://10.7.10.2:9000/todos", { title: content })
         .then(res => {
           this.todos.push(res.data);
         });
     },
-    // 选中所有
+    // 选中所有 updata  .put()  .patch()
     selectAllJudge() {
-      if (this.todos.every(todo => todo.completed)) {
-        this.todos.forEach(todo => (todo.completed = false));
-      } else if (this.todos.some(todo => todo.completed)) {
-        this.todos.forEach(todo => (todo.completed = true));
+      const everys = this.todos.every(todo => todo.completed);
+      const somes = this.todos.some(todo => todo.completed);
+      const noEverys = this.todos.every(todo => !todo.completed);
+
+      const trueTodos = this.todos.filter(todo => todo.completed);
+      const trueIds = trueTodos.map(todo => todo.id);
+
+      const falseTodos = this.todos.filter(todo => !todo.completed);
+      const falseIds = falseTodos.map(todo => todo.id);
+
+      const todos = this.todos;
+      const ids = todos.map(todo => todo.id);
+
+      if (everys) {
+        const arrPromise = trueIds.map(item =>
+          axios.patch(`http://10.7.10.2:9000/todos/${item}`, {
+            completed: false
+          })
+        );
+        Promise.all(arrPromise).then(() => {
+          this.todos.forEach(todo => (todo.completed = false));
+        });
+      } else if (somes) {
+        const arrPromise = falseIds.map(item =>
+          axios.patch(`http://10.7.10.2:9000/todos/${item}`, {
+            completed: true
+          })
+        );
+        Promise.all(arrPromise).then(() => {
+          this.todos.forEach(todo => (todo.completed = true));
+        });
       } else {
-        this.todos.forEach(todo => (todo.completed = true));
+        const arrPromise = ids.map(item =>
+          axios.patch(`http://10.7.10.2:9000/todos/${item}`, {
+            completed: true
+          })
+        );
+        Promise.all(arrPromise).then(() => {
+          this.todos.forEach(todo => (todo.completed = true));
+        });
       }
     },
-    // 改变内容
+    // 改变内容  updata  .put()  .patch()
     todoContentChange(id, newContent) {
       axios
-        .patch("http://10.7.10.2:9000/todos" + "/" + id, {
-          title: newContent
-        })
+        .patch("http://10.7.10.2:9000/todos" + "/" + id, { title: newContent })
         .then(res => {
           const index = this.todos.findIndex(todo => todo.id == id);
-          this.todos.splice(index, 1, res.data);
+          this.todos.splice(index, 1, tes.data);
         });
     },
-    // 删除
+    // 删除 delete  .post()  .delete()
     todoOut(id) {
       axios.delete("http://10.7.10.2:9000/todos" + "/" + id).then(res => {
         const index = this.todos.findIndex(todo => todo.id == id);
+        const todo = this.todos[index];
         this.todos.splice(index, 1);
       });
     },
-    // 选择
+    // 选择 update  .put() .patch()
     todoSelect(id) {
       const index = this.todos.findIndex(todo => todo.id == id);
       const todo = this.todos[index];
       axios
-        .patch("http://10.7.10.2:9000/todos" + "/" + id, {
+        .patch(`http://10.7.10.2:9000/todos/${id}`, {
           completed: !todo.completed
         })
         .then(res => {
-          this.todos.splice(index, 1, res.data);
+          todo.completed = !todo.completed;
         });
     },
+    // 标题改变
     titleChange(title) {
       switch (this.title) {
         case 0:
@@ -104,9 +138,21 @@ export default {
           break;
       }
     },
+    // 删除已选
     outSelected() {
-      const newTodos = this.todos.filter(todo => !todo.completed);
-      this.todos = newTodos;
+      // todos存放completed=true的项
+      const todos = this.todos.filter(todo => todo.completed);
+      // ids存放completed=true项的id
+      const ids = todos.map(todo => todo.id);
+      // arrPromise是map方法返回的存放着Promise的数组
+      const arrPromise = ids.map(item =>
+        axios.delete(`http://10.7.10.2:9000/todos/${item}`)
+      );
+      // Promise.all()
+      Promise.all(arrPromise).then(() => {
+        const todos = this.todos.filter(todo => !todo.completed);
+        this.todos = todos;
+      });
     }
   },
   computed: {
@@ -124,10 +170,9 @@ export default {
       }
     }
   },
-  watch: {
-  },
+  watch: {},
   created() {
-    // 取出资源.get
+    // 取出资源  read  .get()
     axios
       .get("http://10.7.10.2:9000/todos")
       .then(res => {
